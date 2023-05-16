5341a1668fb5521b2084e7581d46b02c06334dfc
Jeff Raubitschek <jeff@wandb.com>
2023-05-16 15:21:13 -0700

    update hardcoded entity

--- src/wandbot/config.py
+++ src/wandbot/config.py
@@ -1,12 +1,15 @@
+import os
 from types import SimpleNamespace
 
 TEAM = "wandbot"
 PROJECT = "wandbbot"
 JOB_TYPE = "production"
 
+ARTIFACT_ENTITY = os.getenv("WANDB_ENTITY", TEAM)
+
 default_config = SimpleNamespace(
-    faiss_artifact="jeffr/wandb_docs_bot_dev/faiss_store:latest",
-    hyde_prompt_artifact="jeffr/wandb_docs_bot_dev/hyde_prompt:latest",
-    chat_prompt_artifact="jeffr/wandb_docs_bot_dev/system_prompt:latest",
+    faiss_artifact=f"{ARTIFACT_ENTITY}/wandb_docs_bot_dev/faiss_store:latest",
+    hyde_prompt_artifact=f"{ARTIFACT_ENTITY}/wandb_docs_bot_dev/hyde_prompt:latest",
+    chat_prompt_artifact=f"{ARTIFACT_ENTITY}/wandb_docs_bot_dev/system_prompt:latest",
     model_name="gpt-4",
 )
